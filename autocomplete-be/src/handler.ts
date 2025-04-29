56248c675acd6b36a0a38cf7dc9b0c31305ebbb1
Melnychuk <milana1726@gmail.com>
2025-04-30 00:35:48 +0200

    feat: implement handler function

--- autocomplete-be/src/handler.ts
+++ autocomplete-be/src/handler.ts
@@ -1,5 +1,31 @@
-import { IncomingMessage, ServerResponse } from "http";
+import { IncomingMessage, ServerResponse } from 'http';
+import { LoadCities } from './load-cities';
 
-export const handler = async (req: IncomingMessage, res: ServerResponse) => {
+export const handler = async (
+    req: IncomingMessage,
+    res: ServerResponse,
+    cities: LoadCities
+): Promise<void> => {
+    const parseURL = new URL(req.url ?? '', `http://${req.headers.host}`);
+    if (req.method !== 'GET' || parseURL.pathname !== '/') {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Not Found' }));
+        return;
+    }
 
+    const prefix = parseURL.searchParams.get('complete');
+    if (!prefix) {
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Missing complete parameter' }));
+        return;
+    }
+
+    const suggestions = cities.getSuggestions(prefix);
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'public, max-age=3600',
+        'Last-Modified': cities.getLastModified(),
+    });
+
+    res.end(JSON.stringify({ suggestions }));
 };
--- autocomplete-be/src/server.ts
+++ autocomplete-be/src/server.ts
@@ -1,8 +1,15 @@
-import { createServer } from 'http';
-import { handler } from "./handler";
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { handler } from './handler';
+import path from 'path';
+import { LoadCities } from './load-cities';
 
 const PORT = 3000;
 
-const server = createServer(handler);
+const citiesFilePath = path.join(__dirname, 'cities.json');
+const cities: LoadCities = new LoadCities(citiesFilePath);
 
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+const server = createServer((req: IncomingMessage, res: ServerResponse) =>
+    handler(req, res, cities)
+);
+
+server.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
