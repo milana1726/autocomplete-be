30580851e31ea25d51cf025997201f3178d4a47c
Melnychuk <milana1726@gmail.com>
2025-07-04 00:19:32 +0200

    refactor: move files from autocomplete-be to root

---
+++ src/load-cities.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import { createAutoComplete } from '../auto-complete/';
+
+export class LoadCities {
+    private cities: string[] = [];
+    private autoComplete: (prefix: string | null) => string[] = () => [];
+
+    constructor(private citiesFilePath: string) {
+        this.loadCities();
+    }
+
+    private loadCities(): void {
+        const data = fs.readFileSync(this.citiesFilePath, 'utf-8');
+        this.cities = JSON.parse(data);
+        this.autoComplete = createAutoComplete(this.cities);
+    }
+
+    public getSuggestions(prefix: string | null): string[] {
+        return this.autoComplete(prefix);
+    }
+}
